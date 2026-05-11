4adce7b6de715dd12c3326b95827a90c132c8695
alonepro098 <aloneee772@gmail.com>
2026-05-11 15:01:37 +0530

    Update bio.py

--- bio.py
+++ bio.py
@@ -455,8 +455,8 @@ async def setduration_handler(client: Client, message):
     try:
         seconds = _parse_duration(arg)
     except Exception:
-        return await client.send_message(chat_id, \"**Invalid duration. Use seconds or 30m / 2h / 1d / 0.**\")
-
+        return await client.send_message(chat_id, "**Invalid duration. Use seconds or 30m / 2h / 1d / 0.**")
+        
     await update_config(chat_id, penalty_duration=seconds)
     await client.send_message(chat_id, f\"**Penalty duration set to {seconds if seconds>0 else 'Permanent'}**\")
 
