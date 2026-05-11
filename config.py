e631b92503eb6cd6df86ece8f5e547d4bd9e1ec3
alonepro098 <aloneee772@gmail.com>
2026-05-11 14:45:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ import re
 import os
 
 # Get environment variables (required for Heroku)
-API_ID = int(os.environ.get("API_ID", "12345678"))  # Your Telegram API ID
-API_HASH = os.environ.get("API_HASH", "12345678abcd")  # Your Telegram API Hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7267436522:XXXXXXXXXXXXXXXXXX")  # Your Bot Token
+API_ID = int(os.environ.get("API_ID", "29282829"))  # Your Telegram API ID
+API_HASH = os.environ.get("API_HASH", "bj7285v7766828999167f46288")  # Your Telegram API Hash
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8399314956:AAE78Z_rKnILuJJH1vtOp8Qhv5OWXgpMREs")  # Your Bot Token
 
 # MongoDB connection URI
-MONGO_URI = os.environ.get("MONGO_URI", "your_mongodb_url")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://PurviBots:PublicMongo@cluster0.gy2adez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DEFAULT_WARNING_LIMIT = 3
 DEFAULT_PUNISHMENT = "mute"  # Options: "mute", "ban"
